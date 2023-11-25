517eed6bf1d50128b520c1ec6a4e5839369e5c46
Separatehhh23 <separatehhh23@gmail.com>
2023-11-25 14:32:40 +0100

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+import tailwind from "@astrojs/tailwind";
+import vue from "@astrojs/vue";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://www.pylinker.com',
+  integrations: [mdx(), sitemap(), tailwind(), vue()],
+  output: "hybrid",
+  adapter: vercel()
 });
