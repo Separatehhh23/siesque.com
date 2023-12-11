693c56b1c59c130ae0cac76c06f57dac855538da
Separatehhh23 <separatehhh23@gmail.com>
2023-12-11 20:59:28 +0100

    Made post-1.mdx more complex, fixed [...tag].astro being empty thus giving an error when building

--- src/env.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference path="../.astro/types.d.ts" />
-interface ImportMetaEnv {
-    readonly CONTENTFUL_SPACE_ID: string;
-    readonly CONTENTFUL_DELIVERY_TOKEN: string;
-    readonly CONTENTFUL_PREVIEW_TOKEN: string;
-}
