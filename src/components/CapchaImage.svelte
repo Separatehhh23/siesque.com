89f7d5f168f134a46950b58f0a3c869395dcf4db
Separatehhh23 <separatehhh23@gmail.com>
2024-01-04 22:53:35 +0100

    Remade post-1.mdx, made Footer.astro dynamicer

--- src/components/CapchaImage.svelte
+++ src/components/CapchaImage.svelte
@@ -1,15 +1,16 @@
 <script lang="ts">
-    const handleClick = () =>  {
+    const handleClick = (): null =>  {
         localStorage.setItem("iscastor", JSON.stringify(true));
-        alert('Mentira! eres un castor');
+        alert('Mentira! Eres un castor');
         return null;
     }
 </script>
 
-<button on:click={handleClick()}>
+<button on:click={handleClick}>
     <img
-      src="https://www.startpage.com/av/proxy-image?piurl=https%3A%2F%2Fstatic1.squarespace.com%2Fstatic%2F58594172893fc07d5b88dd15%2Ft%2F5acf3555f950b77b26f0ef7c%2F1523529051523%2Ftraffic-lights.jpg&sp=1704394558T9477e99b6cb7ce40112fd869d53cfb84c60754a1baca1c3c24f8b4c066ccff1c"
+      src="src/assets/trafficLight.jpeg"
       alt="semaforo"
-      class="resize"
+      height="250"
+      width="250"
     />
 </button>
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,8 +1,11 @@
 /// <reference path="../.astro/types.d.ts" />
+
+interface ImportMetaEnv {
+  readonly PUBLIC_ENV: "beta" | "dev" | "prod" | "local";
+  readonly SUPABASE_URL: string;
+  readonly SUPABASE_ANON_KEY: string;
+}
+
 interface ImportMeta {
-  readonly env: {
-    readonly PUBLIC_ENV: "beta" | "dev" | "prod" | "local";
-    readonly SUPABASE_URL: string;
-    readonly SUPABASE_ANON_KEY: string;
-  };
+  readonly env: ImportMetaEnv;
 }
