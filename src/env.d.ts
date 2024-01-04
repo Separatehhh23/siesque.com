6c0cf0f1007d112609187f5cd98b74f4f9c9ce95
Separatehhh23 <separatehhh23@gmail.com>
2024-01-04 20:01:58 +0100

    Added cpacha

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,8 @@
-/// <reference types="astro/client" />
 /// <reference path="../.astro/types.d.ts" />
+interface ImportMeta {
+  readonly env: {
+    readonly PUBLIC_ENV: "beta" | "dev" | "prod" | "local";
+    readonly SUPABASE_URL: string;
+    readonly SUPABASE_ANON_KEY: string;
+  };
+}
---
+++ src/pages/api/auth/signin.ts
@@ -0,0 +1,65 @@
+import type { APIRoute } from "astro";
+import { supabase } from "../../../lib/supabase";
+import type { Provider } from "@supabase/supabase-js";
+
+export const POST: APIRoute = async ({ request, cookies, redirect }) => {
+  const formData = await request.formData();
+  const email = formData.get("email")?.toString();
+  const password = formData.get("password")?.toString();
+  const provider = formData.get("provider")?.toString();
+  const validProviders = ["discord"];
+
+  let url = "https://www.pylinker.com";
+  switch (import.meta.env.PUBLIC_ENV) {
+    case "local":
+      url = "https://localhost:4321";
+      break;
+    case "beta":
+      url = "https://beta.pylinker.com";
+      break;
+    case "dev":
+      url = "https://dev.pylinker.com";
+      break;
+    case "prod":
+      break;
+    default:
+      break;
+  }
+
+  if (provider && validProviders.includes(provider)) {
+    const { data, error } = await supabase.auth.signInWithOAuth({
+      provider: provider as Provider,
+      options: {
+        redirectTo: `${url}/api/auth/callback`,
+      },
+    });
+
+    if (error) {
+      return new Response(error.message, { status: 500 });
+    }
+
+    return redirect(data.url);
+  }
+
+  if (!email || !password) {
+    return new Response("Email and password are required", { status: 400 });
+  }
+
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email,
+    password,
+  });
+
+  if (error) {
+    return new Response(error.message, { status: 500 });
+  }
+
+  const { access_token, refresh_token } = data.session;
+  cookies.set("sb-access-token", access_token, {
+    path: "/",
+  });
+  cookies.set("sb-refresh-token", refresh_token, {
+    path: "/",
+  });
+  return redirect("/");
+};
