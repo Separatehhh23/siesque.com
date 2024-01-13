b20b82a85e737fc26b0fafd237eeb9d5e66284cc
Separatehhh23 <separatehhh23@gmail.com>
2024-01-13 23:39:17 +0100

    Fixxed bugs in auth flow

--- src/pages/api/auth/signin.ts
+++ src/pages/api/auth/signin.ts
@@ -4,12 +4,14 @@ import type { Provider } from "@supabase/supabase-js";
 
 export const POST: APIRoute = async ({ request, cookies, redirect }) => {
   const formData = await request.formData();
-  const email = formData.get("email")?.toString();
-  const password = formData.get("password")?.toString();
   const provider = formData.get("provider")?.toString();
-  const validProviders = ["discord"];
+  const validProviders = ["github"];
 
-  let url = "https://www.pylinker.com";
+  let url:
+    | "https://localhost:4321"
+    | "https://beta.pylinker.com"
+    | "https://dev.pylinker.com"
+    | "https://www.pylinker.com";
   switch (import.meta.env.PUBLIC_ENV) {
     case "local":
       url = "https://localhost:4321";
@@ -21,6 +23,7 @@ export const POST: APIRoute = async ({ request, cookies, redirect }) => {
       url = "https://dev.pylinker.com";
       break;
     case "prod":
+      url = "https://www.pylinker.com";
       break;
     default:
       break;
@@ -30,7 +33,10 @@ export const POST: APIRoute = async ({ request, cookies, redirect }) => {
     const { data, error } = await supabase.auth.signInWithOAuth({
       provider: provider as Provider,
       options: {
-        redirectTo: `${url}/api/auth/callback`,
+        redirectTo: `${
+          // @ts-ignore
+          typeof url !== "undefined" ? url : "https://beta.pylinker.com"
+        }/api/auth/callback`,
       },
     });
 
@@ -40,26 +46,5 @@ export const POST: APIRoute = async ({ request, cookies, redirect }) => {
 
     return redirect(data.url);
   }
-
-  if (!email || !password) {
-    return new Response("Email and password are required", { status: 400 });
-  }
-
-  const { data, error } = await supabase.auth.signInWithPassword({
-    email,
-    password,
-  });
-
-  if (error) {
-    return new Response(error.message, { status: 500 });
-  }
-
-  const { access_token, refresh_token } = data.session;
-  cookies.set("sb-access-token", access_token, {
-    path: "/",
-  });
-  cookies.set("sb-refresh-token", refresh_token, {
-    path: "/",
-  });
   return redirect("/");
 };
