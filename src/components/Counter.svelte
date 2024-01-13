516f7ae50dde0d71b0cd58eef72516c37f3a6020
Separatehhh23 <separatehhh23@gmail.com>
2024-01-13 23:20:30 +0100

    If count hits 69 it redirects to /capcha

--- src/components/Counter.svelte
+++ src/components/Counter.svelte
@@ -2,5 +2,7 @@
     import { count, incrementCount as increment, resetCount as reset } from "@/stores";
 </script>
 
-<button class="btn btn-primary" on:click={increment}>Count is {$count}</button>
+<!-- <button class="btn btn-primary" on:click={increment}>Count is {$count}</button> -->
+
+<a class="btn btn-primary" href={$count === 69 ? "/capcha" : null} on:click={increment}>Count is {$count}</a>
 <button class="btn btn-secondary" on:click={reset}>Reset count</button>
