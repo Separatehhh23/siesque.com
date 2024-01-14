a65420a0b49fa994e2e5b0c08090cb35349c3155
Separatehhh23 <separatehhh23@gmail.com>
2024-01-14 14:01:42 +0100

    Improved capcha

---
+++ src/components/Capcha.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+    let clicked = false;
+
+    function handleClick() {
+        localStorage.setItem("iscastor", JSON.stringify(true));
+        alert("Mentira! Eres un castor");
+    }
+</script>
+
+<div
+    class="flex size-[26rem] flex-col justify-between bg-base-200 shadow-2xl"
+>
+    <p class="p-2 font-bold text-secondary">
+        Pulsa todos los semaforos para comprobar que no eres un castor
+    </p>
+    <div class="flex flex-col items-center justify-center">
+        <div class="grid w-96 grid-cols-3 grid-rows-3">
+            {#each Array.from({ length: 9 }, (value, index) => index) as i}
+                <div class="p-2">
+                    <button class="cursor-pointer" on:click={() => clicked = true}>
+                        <img src="trafficLight.jpeg" alt="semaforo" width="128" />
+                    </button>
+                </div>
+            {/each}
+        </div>
+    </div>
+    <div class="flex flex-row justify-end pb-4 pr-6">
+        <button class="btn btn-primary" on:click={clicked ? handleClick : null}>Verify</button>
+    </div>
+</div>
--- src/components/NavBar/ThemeController.svelte
+++ src/components/NavBar/ThemeController.svelte
@@ -4,17 +4,14 @@ $: localStorage.setItem("isdark", JSON.stringify(isDark));
 </script>
 
 
-<li>
-    <div class="justify-between">
-        <div class="text-text">Theme</div>
-        <label class="cursor-pointer grid place-items-center">
-            <input
-                    type="checkbox"
-                    checked={isDark}
-                    on:change={() => isDark = !isDark}
-                    class="toggle theme-controller bg-amber-300 border-sky-400 [--tglbg:theme(colors.sky.500)] checked:bg-blue-300 checked:border-blue-800 checked:[--tglbg:theme(colors.blue.900)] row-start-1 col-start-1 col-span-2"
-            />
-        </label>
-    </div>
-</li>
-
+<div class="justify-between">
+    <div class="text-text">Theme</div>
+    <label class="cursor-pointer grid place-items-center">
+        <input
+            type="checkbox"
+            checked={isDark}
+            on:change={() => isDark = !isDark}
+            class="toggle theme-controller bg-amber-300 border-sky-400 [--tglbg:theme(colors.sky.500)] checked:bg-blue-300 checked:border-blue-800 checked:[--tglbg:theme(colors.blue.900)] row-start-1 col-start-1 col-span-2"
+        />
+    </label>
+</div>
