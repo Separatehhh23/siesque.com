ff7d66168eea516ed7853e2be04b3e4708320384
Separatehhh23 <separatehhh23@gmail.com>
2024-03-15 21:54:04 +0100

    ...

--- src/components/Rili/RiliList.tsx
+++ src/components/Rili/RiliList.tsx
@@ -1,33 +1,39 @@
-import {
-  Table,
-  TableBody,
-  TableCaption,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
+import type { RiliArray } from "@/types";
 
-export default function RiliList({ rilis }: { rilis: Array<object> }) {
+export default function RiliList({ rilis }: { rilis: RiliArray }) {
   return (
-    <Table>
-      <TableCaption>A list of your recent invoices.</TableCaption>
-      <TableHeader>
-        <TableRow>
-          <TableHead className="w-[100px]">Invoice</TableHead>
-          <TableHead>Status</TableHead>
-          <TableHead>Method</TableHead>
-          <TableHead className="text-right">Amount</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        <TableRow>
-          <TableCell className="font-medium">INV001</TableCell>
-          <TableCell>Paid</TableCell>
-          <TableCell>Credit Card</TableCell>
-          <TableCell className="text-right">$250.00</TableCell>
-        </TableRow>
-      </TableBody>
-    </Table>
+    <>
+      <div className="absolute flex w-screen flex-row justify-end">
+        <div className="pr-8 pt-2">
+          <button className="btn btn-ghost cursor-pointer rounded-md bg-base-200">
+            <p className="p-0.5 text-primary accent-accent">Change</p>
+          </button>
+        </div>
+      </div>
+      <div className="flex h-screen w-screen flex-row justify-center pt-32">
+        <div className="min-w-1/4 rounded-xl">
+          <div className="overflow-x-auto">
+            <table className="table">
+              <thead>
+                <tr>
+                  <th className="text-primary">Rank</th>
+                  <th className="text-primary">Name</th>
+                  <th className="text-primary">Amount</th>
+                </tr>
+              </thead>
+              <tbody>
+                {rilis.map((rili) => (
+                  <tr>
+                    <th className="text-secondary">{rili.id}</th>
+                    <td>{rili.name}</td>
+                    <td>{rili.amount}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,35 +1,6 @@
 import type { Dispatch, SetStateAction } from "react";
 import type { CollectionEntry } from "astro:content";
 
-interface ArrayFunctions {
-  map: () => Array<any>;
-  filter: () => Array<any>;
-  forEach: () => Array<any>;
-  reduce: () => Array<any>;
-  sort: () => Array<any>;
-  slice: () => Array<any>;
-  join: () => Array<any>;
-  pop: () => Array<any>;
-  push: () => Array<any>;
-  shift: () => Array<any>;
-  unshift: () => Array<any>;
-  reverse: () => Array<any>;
-  concat: () => Array<any>;
-  includes: () => Array<any>;
-  indexOf: () => Array<any>;
-  lastIndexOf: () => Array<any>;
-  toString: () => Array<any>;
-  toLocaleString: () => Array<any>;
-  valueOf: () => Array<any>;
-  length: number;
-  splice: () => Array<any>;
-  fill: () => Array<any>;
-  copyWithin: () => Array<any>;
-  find: () => Array<any>;
-  findIndex: () => Array<any>;
-  flat: () => Array<any>;
-}
-
 interface BlogEntry extends CollectionEntry<"blog">, ArrayFunctions {
   id: string;
   slug: string;
@@ -48,24 +19,6 @@ interface ModalElement extends HTMLElement {
   showModal(): void;
 }
 
-type Pick<T, K extends keyof T> = { [P in K]: T[P] };
-type Exclude<T, U> = T extends U ? never : T;
-type Omit<T, K extends keyof any> = Pick<T, Exclude<keyof T, K>>;
-type Modify<T, R> = Omit<T, keyof R> & R;
-type Record<K extends keyof any, T> = { [P in K]: T };
-type Params = Record<string, string | undefined>;
+type RiliArray = Array<Object<number, string, number>>;
 
-type SetState<T> = Dispatch<SetStateAction<T>>;
-
-export type {
-  BlogEntry,
-  Pick,
-  Exclude,
-  Modify,
-  Omit,
-  Record,
-  Params,
-  ModalElement,
-  ArrayFunctions,
-  SetState,
-};
+export type { BlogEntry, Params, ModalElement, RiliArray };
