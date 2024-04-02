f39dae45a351746952c8026dcb1d7aefec0ec96b
Separatehhh23 <separatehhh23@gmail.com>
2024-04-02 20:17:39 +0200

    Emergenmcy push alpha version

--- src/components/Rili/RiliList.tsx
+++ src/components/Rili/RiliList.tsx
@@ -1,39 +1,50 @@
 import type { RiliArray } from "@/types";
+import { TracingBeam } from "../ui/tracing-beam";
+import "@/styles/RiliList.scss";
 
-export default function RiliList({ rilis }: { rilis: RiliArray }) {
+interface Props {
+  rilis: RiliArray;
+}
+
+export default function RiliList(props: Props) {
+  const rilis = props.rilis;
+
   return (
     <>
-      <div className="absolute flex w-screen flex-row justify-end">
+      <div className="absolute z-20 flex w-screen flex-row justify-end">
         <div className="pr-8 pt-2">
           <button className="btn btn-ghost cursor-pointer rounded-md bg-base-200">
             <p className="p-0.5 text-primary accent-accent">Change</p>
           </button>
         </div>
       </div>
-      <div className="flex h-screen w-screen flex-row justify-center pt-32">
-        <div className="min-w-1/4 rounded-xl">
-          <div className="overflow-x-auto">
-            <table className="table">
-              <thead>
-                <tr>
-                  <th className="text-primary">Rank</th>
-                  <th className="text-primary">Name</th>
-                  <th className="text-primary">Amount</th>
-                </tr>
-              </thead>
-              <tbody>
-                {rilis.map((rili) => (
+      <TracingBeam>
+        {/*<div className="flex h-screen w-screen flex-row justify-center pt-32">*/}
+        <div className="custom--main-list relative mx-auto max-w-2xl pt-4 antialiased">
+          <div className="min-w-1/4 rounded-xl">
+            <div className="overflow-x-auto">
+              <table className="table">
+                <thead>
                   <tr>
-                    <th className="text-secondary">{rili.id}</th>
-                    <td>{rili.name}</td>
-                    <td>{rili.amount}</td>
+                    <th className="text-primary">Rank</th>
+                    <th className="text-primary">Name</th>
+                    <th className="text-primary">Amount</th>
                   </tr>
-                ))}
-              </tbody>
-            </table>
+                </thead>
+                <tbody>
+                  {rilis.map((rili) => (
+                    <tr>
+                      <th className="text-secondary">{rili.id}</th>
+                      <td>{rili.name}</td>
+                      <td>{rili.amount}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
-      </div>
+      </TracingBeam>
     </>
   );
 }
