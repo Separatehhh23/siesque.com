6a2d27a0d159556fcdf6b2c8d775fd4a5b025377
Separatehhh23 <separatehhh23@gmail.com>
2024-04-25 21:12:06 +0200

    Rili????

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -52,7 +52,7 @@ export default {
   },
 };
 
-function addVariablesForColors({ addBase, theme }: any) {
+function addVariablesForColors({ addBase, theme }) {
   let allColors = flattenColorPalette(theme("colors"));
   let newVars = Object.fromEntries(
       Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
