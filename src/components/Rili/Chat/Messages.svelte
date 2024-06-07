0888518cf2b1ed510a5d2a9709c047191cadec1b
Separatehhh23 <separatehhh23@gmail.com>
2024-06-08 01:31:17 +0200

    Add word to dictionary, improve rili chat

--- src/components/Rili/Chat/Messages.svelte
+++ src/components/Rili/Chat/Messages.svelte
@@ -8,12 +8,13 @@
   let username: string;
   let messages = [];
   let unsubscribe: () => void;
+  let sending = false;
+  let latestMessage: string;
 
   onMount(async () => {
-    const resultList = await pb.collection("messages").getList(1, 50, {
+    const resultList = await pb.collection("messages").getList(1, 10000, {
       sort: "created",
     });
-    messages = resultList.items;
 
     unsubscribe = await pb
       .collection("messages")
@@ -22,6 +23,10 @@
           messages = [...messages, record];
         }
       });
+
+    messages = resultList.items.slice(
+      Math.max(resultList.items.length - 20, 0),
+    );
   });
 
   onDestroy(() => {
@@ -29,15 +34,29 @@
   });
 
   async function sendMessage() {
-    await pb.collection("messages").create({
-      text: newMessage,
-      author: username,
+    if (!sending && latestMessage !== newMessage) {
+      sending = true;
+      latestMessage = newMessage;
+      await pb.collection("messages").create({
+        text: newMessage,
+        author: username,
+      });
+      setTimeout(() => {
+        sending = false;
+      }, 1000);
+    }
+  }
+
+  async function showAll() {
+    const resultList = await pb.collection("messages").getList(1, 10000, {
+      sort: "created",
     });
+    messages = resultList.items;
   }
 </script>
 
-<div class="flex h-screen w-screen flex-row justify-center">
-  <div class="mt-8 w-2/3 pt-8">
+<div class="flex min-h-screen w-screen flex-row justify-center">
+  <div class="mb-8 mt-8 w-2/3 pb-8 pt-8">
     <div class="flex flex-col gap-2" use:autoAnimate>
       {#each messages as message (message.id)}
         <div class="rounded-xl bg-base-200 p-2">
@@ -45,25 +64,32 @@
           <small>Sent by @{message.author}</small>
         </div>
       {/each}
-      <form
-        on:submit|preventDefault={sendMessage}
-        class="join rounded-xl bg-base-200 p-2"
-      >
-        <input
-          placeholder="Message"
-          type="text"
-          class="input join-item"
-          bind:value={newMessage}
-        />
-        <div class="join-item h-full w-1 bg-base-200" />
-        <input
-          placeholder="Username"
-          type="text"
-          class="input join-item"
-          bind:value={username}
-        />
-        <button type="submit" class="btn btn-primary join-item">Send</button>
-      </form>
+      <div class="flex w-full flex-row gap-1">
+        <form
+          on:submit|preventDefault={sendMessage}
+          class="join justify-self-start rounded-xl bg-base-200 p-2"
+        >
+          <input
+            placeholder="Message"
+            type="text"
+            class="input join-item"
+            bind:value={newMessage}
+          />
+          <div class="join-item h-full w-1 bg-base-200" />
+          <input
+            placeholder="Username"
+            type="text"
+            class="input join-item"
+            bind:value={username}
+          />
+          <button type="submit" class="btn btn-primary join-item">Send</button>
+        </form>
+        <div class="justify-self-end rounded-xl bg-base-200 p-2">
+          <button on:click|preventDefault={showAll} class="btn btn-ghost"
+            >Show all</button
+          >
+        </div>
+      </div>
     </div>
   </div>
 </div>
