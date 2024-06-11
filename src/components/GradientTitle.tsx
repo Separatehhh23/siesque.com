0550b17c5f34699584504d3e513f57b61f28e327
Separatehhh23 <separatehhh23@gmail.com>
2024-06-11 19:15:09 +0200

    New title

--- src/components/GradientTitle.tsx
+++ src/components/GradientTitle.tsx
@@ -1,27 +1,25 @@
 import { cn } from "@/lib/utils";
-import type { FC, ReactNode } from "react";
+import type { ReactNode } from "react";
 
 interface Props {
-  text: [string, string, string];
+  children: ReactNode;
   className?: string;
 }
 
-const GradientTitle: FC<Props> = ({ text, className }) => (
+const GradientTitle = ({ children, className }: Props) => (
   <div className={className}>
     <div className="flex w-[125%] flex-col bg-gradient-to-br from-cyan-500 to-indigo-500 bg-clip-text text-transparent">
-      <Title className="justify-start">{text[0]}</Title>
-      <Title className="justify-center">{text[1]}</Title>
-      <Title className="justify-end">{text[2]}</Title>
+      <Title>{children}</Title>
     </div>
   </div>
 );
 
 interface TitleProps {
-  className: string;
+  className?: string;
   children: ReactNode;
 }
 
-const Title: FC<TitleProps> = ({ children, className }) => (
+const Title = ({ children, className }: TitleProps) => (
   <div className={cn("inline-flex", className)}>
     <h1 className="inline-block font-extrabold sm:text-4xl md:text-8xl xl:text-[10rem]">
       {children}
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -51,6 +51,11 @@ export default {
     function ({ matchUtilities, theme }: any) {
       matchUtilities(
         {
+          "bg-grid-large": (value: any) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="64" height="64" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
+            )}")`
+          }),
           "bg-grid": (value: any) => ({
             backgroundImage: `url("${svgToDataUri(
               `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
